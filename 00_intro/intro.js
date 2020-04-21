7e23180d19694c91392d08fda2ae4695044b86cd
unknown <angel202001>
2020-04-21 16:09:33 -0400

    Agregando README

---
+++ 00_intro/intro.js
@@ -0,0 +1,14 @@
+    const inputColor = document.querySelector('#color');
+    const btnCambiarColor = document.querySelector('#cambiar_color');
+
+    btnCambiarColor.addEventListener('click', () => {
+        let color = inputColor.value;
+        
+
+       cambiarColorFondo(color);
+    });
+
+    function cambiarColorFondo(color) {
+        document.body.style.backgroundColor = color;
+    }
+    
