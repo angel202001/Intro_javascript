e999386786e62b074956936f7a0eb928048aefc0
unknown <angel202001>
2020-06-12 15:38:33 -0400

    practica

---
+++ Extenciones de archivos/prueba_base_datos.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE infotep;
+
+USE infotep;
+
+CREATE TABLE participantes(
+    id INT,
+    nombre VARCHAR(25),
+    apellido VARCHAR(25),
+    cedula VARCHAR(13),
+    genero CHAR(1),-- M (masculino) o F (femenino)
+    edad SMALLINT 
+);
